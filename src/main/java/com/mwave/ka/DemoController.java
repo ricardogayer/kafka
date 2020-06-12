a714f107c6c2737b5eddd6a102e74ea4ed20aa59
rrgayer <zujpyP-wapxuk-ficgy4>
2020-06-12 16:12:24 -0300

    key value

--- src/main/java/com/mwave/ka/DemoController.java
+++ src/main/java/com/mwave/ka/DemoController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 public class DemoController {
 
@@ -14,7 +16,7 @@ public class DemoController {
 
     @PostMapping("/publish")
     public String publishKafka(@RequestBody Book book) {
-        kafkaTemplate.send(TOPIC,book);
+        kafkaTemplate.send(TOPIC,UUID.randomUUID().toString(),book);
         return "Message in kafka";
     }
 
